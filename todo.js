c271c8f7c7fd6f86e70fda9f27b87b1a29f456b5
pkarthik88 <vprasannakarthik@gmail.com>
2023-02-01 23:23:01 +0800

    Starter Code for Assignment-2

---
+++ todo.js
@@ -0,0 +1,57 @@
+// Some code snippets have been provided to you for ease of coding.
+// You can choose to remove or change any of them to suit your needs.
+
+var outstandingtasks=[];
+var finishedtasks=[];
+var maxtaskid=0;
+
+function bootstrap()
+{
+    // Code for Q7 starts here. This code restores the values
+    // of variables to their previous values (i.e., before browser
+    // was closed). 
+
+    // Code for Q7 ends here.
+}
+function addfunction()
+{
+    // Code for Q2 starts here. This function uses DOM read
+    // to get the values of HTML fields. Subsequently, it
+    // adds them to the JS variable called outstandingtasks. 
+    // You are also required to save the contents of this variable
+    // in a JS cookie (Q7). 
+
+
+
+    //Code for Q2 ends here.
+    console.log(outstandingtasks);
+}
+
+function finishfunction()
+{
+    // Code for Q3 starts here. This function uses DOM read to
+    // get the serial number from the user. Subsequently, it
+    // searches/finds the Task matching the serial number and
+    // deletes the task from outstandingtasks. Do not forget to
+    // add the task to finished tasks before deleting it.
+
+
+    // Code for Q3 ends.
+    console.log(outstandingtasks);
+}
+
+function displayfunction()
+{
+    // Code for Q4 starts here. This function identifies the HTML
+    // element corresponding to the Tables for outstanding 
+    // and finished tasks. You must create the table by adding rows,
+    // columns, and finally populate the text in the table. Code
+    // for Outstanding tasks and finished tasks is similar. Use
+    // the Demo code used in class as a starting point for table
+    // creation using JS.
+
+
+    
+    // Code for Q4 ends.
+}
+
